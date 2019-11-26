90cb91ba8e0121fa3d04cea4c8205ff2f8598c1d
Gabriel Martínez <gabomartz@gmail.com>
2019-11-26 13:50:24 -0300

    commit

---
+++ precache-manifest.67eb3dadd1bebdd249e58327c3db7ace.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2c3b8a97f2528d5684e1dfefe26004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202ac42545daf99fc970",
+    "url": "/static/css/main.52ca3395.chunk.css"
+  },
+  {
+    "revision": "8dd527cca0dc15569cd2",
+    "url": "/static/js/2.c207685e.chunk.js"
+  },
+  {
+    "revision": "202ac42545daf99fc970",
+    "url": "/static/js/main.bd99ffdd.chunk.js"
+  },
+  {
+    "revision": "968a00975bea42f1c816",
+    "url": "/static/js/runtime-main.cfda98c1.js"
+  },
+  {
+    "revision": "db998f451023182e18f3f0712d5dc0cd",
+    "url": "/static/media/Brique.db998f45.png"
+  },
+  {
+    "revision": "72455bc06e27d0e0762ded033754252e",
+    "url": "/static/media/Krato.72455bc0.png"
+  },
+  {
+    "revision": "9b40b2db0f579fe7374485f830dccd72",
+    "url": "/static/media/Quarvy.9b40b2db.png"
+  },
+  {
+    "revision": "5a1b65c2df52185ba7b986abb34378be",
+    "url": "/static/media/QuarvyApp.5a1b65c2.png"
+  },
+  {
+    "revision": "0bdbef7cae469a33859dcab34cd60674",
+    "url": "/static/media/Vleeko Pagos.0bdbef7c.png"
+  },
+  {
+    "revision": "63796c8f2a1b45431482db2f66743ea8",
+    "url": "/static/media/asd.63796c8f.png"
+  },
+  {
+    "revision": "35115bf6e1941276e11c2538e9f4e608",
+    "url": "/static/media/me.35115bf6.png"
+  },
+  {
+    "revision": "c964be703264f8b3527233f5212d3733",
+    "url": "/static/media/parrots.c964be70.jpeg"
+  },
+  {
+    "revision": "5a0adbd0186985b272f2f66034bec3cf",
+    "url": "/static/media/sun.5a0adbd0.svg"
+  }
+]);
